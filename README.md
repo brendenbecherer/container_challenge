abe0b93bec4c6f2ee9afc8ed3ed6af52c4ee9455
Bob <bob@example.com>
2024-03-13 18:38:44 +0000

    added app.py, requirements, and added curl to readme

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # container_challenge
 Container Challenge Flask App Exercise
+
+curl -X GET http://localhost:5000
+curl -X POST -H "Content-Type: application/json" -d '{"name": "John Doe", "age": 30, "city": "New York"}' http://localhost:5000
---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask,request,jsonify
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def hello():
+    if request.method == 'GET':
+        return jsonify({"message": "success" })
+    else: 
+        data = request.json
+        return jsonify(data)
+    
+if __name__ == '__main__':
+    app.run()
