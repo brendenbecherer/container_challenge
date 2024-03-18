f4d07a620716422027cf40b89fa820e945e78696
Bob <bob@example.com>
2024-03-18 19:55:18 +0000

    created bash script to deploy clouformation stack

---
+++ ec2deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -x
+
+#variables
+STACK_NAME="container-challenge"
+
+#create stack
+aws cloudformation create-stack --stack-name $STACK_NAME --template-body file://cloudformation.yaml
+
+#wait for stack to complete
+echo "Waiting for stack creation to complete."
+aws cloudformation wait stack-create-complete --stack-name $STACK_NAME
+echo "Stack creation complete."
+
+#get the public dns
+INSTANCE_PUBLIC_DNS=$(aws cloudformation describe-stacks --stack-name $STACK_NAME --query "Stacks[0].Outputs[?OutputKey=='InstancePublicDNS'].OutputValue" --output text)
+echo "Instance Public DNS: $INSTANCE_PUBLIC_DNS"
