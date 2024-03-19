f3455d226877a21588b35fc618c55331f006db50
brenden <test@test.com>
2024-03-19 14:34:21 -0500

    changed app.py to seperate routes for GET and POST

--- app.py
+++ app.py
@@ -2,13 +2,14 @@ from flask import Flask,request,jsonify
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def hello():
-    if request.method == 'GET':
-        return jsonify({"message": "success" })
-    else: 
-        data = request.json
-        return jsonify(data)
+@app.route('/', methods=['GET'])
+def get_message():
+    return jsonify({"message": "success" })
     
+@app.route('/', methods=['POST'])
+def echo_post():
+    data = request.json
+    return jsonify(data)
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
